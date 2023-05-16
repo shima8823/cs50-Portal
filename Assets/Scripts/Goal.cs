9ca08e624a6e9ea52c6d65e545ffe0f8e382ba9c
shima8823 <taiyaki1275@gmail.com>
2023-05-16 21:39:36 +0900

    first commit

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Goal : MonoBehaviour
+{
+    public GameObject textUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // when the player enters the goal
+    void OnTriggerEnter(Collider other)
+    {
+        // if the player enters the goal
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You win!");
+            textUI.SetActive(true);
+        }
+    }
+}
